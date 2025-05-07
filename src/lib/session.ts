9afea2ed0e11ce1b0cd39d3983bd6ff6f4aee165
abellmanuell <mannydev02@gmail.com>
2025-05-07 15:17:27 +0100

    fix: update createSession function parameter type from string to any for improved flexibility

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -26,7 +26,7 @@ export async function decrypt(session: string | undefined = "") {
   }
 }
 
-export async function createSession(userId: string) {
+export async function createSession(userId: any) {
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
   const session = await encrypt({ userId, expiresAt });
   const cookieStore = await cookies();
